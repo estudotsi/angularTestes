bfe5ca00b5d731415de4f73cb6699b86c524d3eb
rogerio.campos <rogerio.campos@cg.df.gov.br>
2023-10-03 22:17:50 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTestes';
+  otp: string = '';
+  config = {
+    allowNumbersOnly: false,
+    length: 10,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '50px',
+      'height': '50px'
+    }
+  };
+
+  onOtpChange(otp: string) {
+    // Validate input to allow only digits and limit to 6 characters
+    console.log("Aqui: ", otp);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgOtpInputModule } from 'ng-otp-input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOtpInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
